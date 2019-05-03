da73b2f29b569bf8f52c05d042db31d276003f62
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2019-05-03 08:26:25 +0100

    Add context to a Principal.
    This means that a principal launched from one context isn’t the same as one launched from another context. This is useful so that a user who has roles in one LTI launch is seen as different from the same user who has different roles in a different context.

--- src/main/java/edu/ksu/lti/launch/oauth/LtiPrincipal.java
+++ src/main/java/edu/ksu/lti/launch/oauth/LtiPrincipal.java
@@ -7,16 +7,30 @@ import java.security.Principal;
 /**
  * Holds the LTI principal. This is designed to support multitenancy.
  * While we could put all the data returned in the LTI launch in here it would make the principal rather heavy. Instead
- * we just give it enough.
+ * we just give it enough. In the future we may want to encode this as a JWT.
  */
 public class LtiPrincipal implements Principal {
 
     private final ToolConsumer consumer;
     private final String user;
+    private final String context;
 
     public LtiPrincipal(ToolConsumer consumer, String user) {
         this.consumer = consumer;
         this.user = user;
+        context = null;
+    }
+
+    /**
+     * Create a new LTI Principal.
+     * @param consumer The LTI tool consumer that this principal was authenticated through.
+     * @param user The user who was authenticated.
+     * @param context The context in the tool consumer that the user was sent from.
+     */
+    public LtiPrincipal(ToolConsumer consumer, String user, String context) {
+        this.consumer = consumer;
+        this.user = user;
+        this.context = context;
     }
 
     @Override
@@ -28,8 +42,15 @@ public class LtiPrincipal implements Principal {
         return user;
     }
 
+    /**
+     * @return This is the context that the LTI was launched from. This isn't the course ID.
+     */
+    public String getContext() {
+        return context;
+    }
+
     public ToolConsumer getConsumer() {
-        return this.getConsumer();
+        return consumer;
     }
 
     public String getTenant() {
@@ -37,6 +58,6 @@ public class LtiPrincipal implements Principal {
     }
 
     public String toString() {
-        return consumer.getInstance()+ ":"+ user;
+        return consumer.getInstance()+ ":"+ user + ((context != null)?":"+ context:"");
     }
 }
